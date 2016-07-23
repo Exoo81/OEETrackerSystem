9bb4624f4539ed7cfd640ec09ca9886783930558
Exoo <marcinpiskoroff@gmail.com>
2016-07-23 22:13:26 +0100

    Mysql and Hibernate connected to application first test creating User table

---
+++ src/main/java/com/exoo/oee/entity/User.java
@@ -0,0 +1,25 @@
+package com.exoo.oee.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue
+	private Integer id_user;
+
+	public Integer getId_user() {
+		return id_user;
+	}
+
+	public void setId_user(Integer id_user) {
+		this.id_user = id_user;
+	}
+	
+	
+	
+
+}
