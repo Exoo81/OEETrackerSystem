b739893ec47babd7ca1dc24912005da64644a2cf
Exoo <marcinpiskoroff@gmail.com>
2016-07-25 19:15:25 +0100

    Spring JPA added + persistance.xml, User andRole Entitys

---
+++ src/main/java/com/exoo/oee/entity/Role.java
@@ -0,0 +1,52 @@
+package com.exoo.oee.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+
+@Entity
+public class Role {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	@Column(name="role_name")
+	private String roleName;
+	
+	@ManyToMany(mappedBy="roles")
+	private List<User> users;
+	
+
+	/** Getters & Setters **/
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+	
+}
--- src/main/java/com/exoo/oee/entity/User.java
+++ src/main/java/com/exoo/oee/entity/User.java
@@ -1,25 +1,61 @@
 package com.exoo.oee.entity;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class User {
 	
 	@Id
 	@GeneratedValue
-	private Integer id_user;
+	private Integer id;
+	
+	private String username;
+	
+	private String password;
+	
+	@ManyToMany
+	@JoinTable
+	private List<Role> roles;
+	
+	
+	/** Getters & Setters **/
 
-	public Integer getId_user() {
-		return id_user;
+	public Integer getId() {
+		return id;
 	}
 
-	public void setId_user(Integer id_user) {
-		this.id_user = id_user;
+	public void setId(Integer id) {
+		this.id = id;
 	}
-	
-	
-	
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
 
 }
